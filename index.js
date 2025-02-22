595582253f05792c51de3b4b8a4de72a1274f659
jan@huggenberg.ch <jan@huggenberg.ch>
2025-02-22 08:56:15 +0100

    added screen wake lock and disabled user zoom.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < defaultPlayerCount; i++) {
         addPlayer(`Player ${alphabet[i]}`, colorHues[i]);
     }
+
+    // Request wake lock
+    requestWakeLock();
 });
 
 function addPlayer(placeholderName, colorHue) {
@@ -16,4 +19,17 @@ function addPlayer(placeholderName, colorHue) {
 
     const playersEl = document.querySelector('.players');
     playersEl.appendChild(player);
+}
+
+async function requestWakeLock() {
+    try {
+        const wakeLock = await navigator.wakeLock.request('screen');
+        console.log('Wake lock is active');
+
+        wakeLock.addEventListener('release', () => {
+            console.log('Wake lock was released');
+        });
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
 }
